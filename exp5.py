b086b927123437934015cf211428aae542b533b6
forceCc <Aries332400@outlook.com>
2024-05-20 22:12:23 +0800

    new file:   exp5-test.py 	modified:   exp5.py

--- exp5.py
+++ exp5.py
@@ -3,93 +3,51 @@ import numpy as np
 from numpy import fft
 from matplotlib import pyplot as plt
 
-img = cv2.imread(r"picture/pepper.tif", cv2.IMREAD_COLOR)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-img_arr = np.array(img)
-img = img_arr.astype(np.double) / 255.0
-
-F = fft.fft2(img, axes=(0, 1))  # 换到频域，信号在低频，噪声在高频
-F = fft.fftshift(F, axes=(0, 1))  # 频谱移到矩阵中心
-
-H, W = F.shape[:2]
-u, v = np.meshgrid(np.arange(H), np.arange(W), indexing="ij")  # 生成矩阵
-
-# k=0.0025剧烈湍流；k=0.001中等湍流；k=0.00025低湍流
-H_turbulence = np.exp(-0.0025 * ((u - H / 2) ** 2 + (v - W / 2) ** 2) ** (5 / 6))
-H_turbulence = np.repeat(H_turbulence[:, :, np.newaxis], 3, axis=2)  # 对应RGB通道
-F = F * H_turbulence
-
-# 傅里叶反变换
-X = fft.ifftshift(F, axes=(0, 1))
-turimg = fft.ifft2(X, axes=(0, 1))
-turimg = abs(turimg)
-turimg = np.uint8(turimg * 255)
-
-
-# def add_gaussian_noise(image, mean=0, sigma=25):
-#     """
-#     为图像添加高斯白噪声
-#     :param image: 输入图像
-#     :param mean: 高斯噪声的均值
-#     :param sigma: 高斯噪声的标准差
-#     :return: 添加噪声后的图像
-#     """
-#     # 生成高斯噪声
-#     gaussian_noise = np.random.normal(mean, sigma, image.shape).astype(np.float32)
-
-#     # 将噪声添加到图像
-#     noisy_image = cv2.add(image.astype(np.float32), gaussian_noise)
-
-#     # 确保像素值在有效范围内
-#     noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
-
-#     return noisy_image
+# 9.19: 湍流模糊退化模型 (turbulence blur degradation model)
+def getDegradedImg(image, Huv):  # 根据退化模型生成退化图像
+    rows, cols = image.shape[:2]  # 图片的高度和宽度
+    # (1) 中心化, centralized 2d array f(x,y) * (-1)^(x+y)
+    mask = np.ones((rows, cols))
+    mask[1::2, ::2] = -1
+    mask[::2, 1::2] = -1
+    imageCen = image * mask
+    # (2) 快速傅里叶变换
+    dftImage = np.zeros((rows, cols, 2), np.float32)
+    dftImage[:, :, 0] = imageCen
+    cv2.dft(
+        dftImage, dftImage, cv2.DFT_COMPLEX_OUTPUT
+    )  # 快速傅里叶变换 (rows, cols, 2)
+    # (4) 构建 频域滤波器传递函数:
+    Filter = np.zeros((rows, cols, 2), np.float32)  # (rows, cols, 2)
+    Filter[:, :, 0], Filter[:, :, 1] = Huv, Huv
+    # (5) 在频率域修改傅里叶变换: 傅里叶变换 点乘 滤波器传递函数
+    dftFilter = dftImage * Filter
+    # (6) 对修正傅里叶变换 进行傅里叶逆变换，并只取实部
+    idft = np.ones((rows, cols), np.float32)  # 快速傅里叶变换的尺寸
+    cv2.dft(
+        dftFilter, idft, cv2.DFT_REAL_OUTPUT + cv2.DFT_INVERSE + cv2.DFT_SCALE
+    )  # 只取实部
+    # (7) 中心化, centralized 2d array g(x,y) * (-1)^(x+y)
+    mask2 = np.ones(dftImage.shape[:2])
+    mask2[1::2, ::2] = -1
+    mask2[::2, 1::2] = -1
+    idftCen = idft * mask2  # g(x,y) * (-1)^(x+y)
+    # (8) 截取左上角，大小和输入图像相等
+    imgDegraded = np.uint8(
+        cv2.normalize(idftCen, None, 0, 255, cv2.NORM_MINMAX)
+    )  # 归一化为 [0,255]
+    # print(image.shape, dftFilter.shape, imgDegraded.shape)
+    return imgDegraded
 
 
-# def motion_process(image_size, motion_angle):
-#     PSF = np.zeros(image_size)
-#     print(image_size)
-#     center_position = (image_size[0] - 1) / 2
-#     print(center_position)
-
-#     slope_tan = math.tan(motion_angle * math.pi / 180)
-#     slope_cot = 1 / slope_tan
-#     if slope_tan <= 1:
-#         for i in range(15):
-#             offset = round(i * slope_tan)  # ((center_position-i)*slope_tan)
-#             PSF[int(center_position + offset), int(center_position - offset)] = 1
-#         return PSF / PSF.sum()  # 对点扩散函数进行归一化亮度
-#     else:
-#         for i in range(15):
-#             offset = round(i * slope_cot)
-#             PSF[int(center_position - offset), int(center_position + offset)] = 1
-#         return PSF / PSF.sum()
-
-
-# def make_blurred(input, PSF, eps):
-#     input_fft = fft.fft2(input)  # 进行二维数组的傅里叶变换
-#     PSF_fft = fft.fft2(PSF) + eps
-#     blurred = fft.ifft2(input_fft * PSF_fft)
-#     blurred = np.abs(fft.fftshift(blurred))
-#     return blurred
-
-
-# def inverse(input, PSF, eps):  # 逆滤波
-#     input_fft = fft.fft2(input)
-#     PSF_fft = fft.fft2(PSF) + eps  # 噪声功率，这是已知的，考虑epsilon
-#     result = fft.ifft2(input_fft / PSF_fft)  # 计算F(u,v)的傅里叶反变换
-#     result = np.abs(fft.fftshift(result))
-#     return result
-
-
-# def wiener(input, PSF, eps, K=0.01):  # 维纳滤波，K=0.01
-#     input_fft = fft.fft2(input)
-#     PSF_fft = fft.fft2(PSF) + eps
-#     PSF_fft_1 = np.conj(PSF_fft) / (np.abs(PSF_fft) ** 2 + K)
-#     result = fft.ifft2(input_fft * PSF_fft_1)
-#     result = np.abs(fft.fftshift(result))
-#     return result
+def turbulenceBlur(img, k=0.001):  # 湍流模糊传递函数
+    # H(u,v) = exp(-k(u^2+v^2)^5/6)
+    M, N = img.shape[1], img.shape[0]
+    u, v = np.meshgrid(np.arange(M), np.arange(N))
+    radius = (u - M // 2) ** 2 + (v - N // 2) ** 2
+    kernel = np.exp(-k * np.power(radius, 5 / 6))
+    return kernel
 
 
 def gaussian_noise(image, mean=0, std_dev=25):
@@ -123,42 +81,33 @@ def wiener_filtering(image, kernel, noise_var):
     return np.clip(restored_image, 0, 255).astype(np.uint8)
 
 
-kernel = np.ones((H, W), np.float32) / (H * W)
+img = cv2.imread(r"picture/pepper.tif", cv2.IMREAD_GRAYSCALE)
 
-gasuss_img = gaussian_noise(turimg)
-# gasuss_img1 = cv2.GaussianBlur(gasuss_img, (3, 3), 0)
-inverse_img = inverse_filtering(gasuss_img.astype(np.uint8), kernel)
+# 生成湍流模糊图像
+HBlur = turbulenceBlur(img, k=0.001)  # 湍流模糊传递函数
+imgBlur = getDegradedImg(img, HBlur)  # 生成湍流模糊图像
 
-noise_var = 25**2  # Assuming noise standard deviation is 25
-wiener_img = wiener_filtering(gasuss_img.astype(np.uint8), kernel, noise_var)
-# wiwner_img = cv2.Wiener2(turimg, (5, 5), (10, 10))
+gaussian_img1 = gaussian_noise(imgBlur)
 
-# PSF = motion_process((W, H), 60)
-# blurred = np.abs(make_blurred(turimg, PSF, 1e-3))
-
-# inverse_img = inverse(turimg, PSF, 1e-3)
-# wiener_img = wiener(turimg, PSF, 1e-3)
+inverse_img = inverse_filtering(gaussian_img1, HBlur)
+wiener_img = wiener_filtering(gaussian_img1, HBlur, imgBlur)
 
 plt.rcParams["font.sans-serif"] = ["SimHei"]
 plt.rcParams["figure.figsize"] = (15, 15)
 plt.subplot(231)
-plt.imshow(img)
+plt.imshow(img, "gray")
 plt.title("原图像")
 plt.axis("off")
 plt.subplot(232)
-plt.imshow(turimg)
+plt.imshow(imgBlur, "gray")
 plt.title("退化图像")
 plt.axis("off")
 plt.subplot(233)
-plt.imshow(gasuss_img)
-plt.title("噪声图像")
+plt.imshow(gaussian_img1, "gray")
+plt.title("高斯噪声图像")
 plt.axis("off")
-plt.subplot(234)
-plt.imshow(inverse_img)
+plt.subplot(236)
+plt.imshow(inverse_img, "gray")
 plt.title("逆滤波图像")
-plt.axis("off")
-plt.subplot(235)
-plt.imshow(wiener_img)
-plt.title("维纳滤波图像")
 plt.axis("off")
 plt.show()
